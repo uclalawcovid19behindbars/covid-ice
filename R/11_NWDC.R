68215a96730411870ff6fa0ac3c2760a5f493ac2
Neal Marquez <nmmarquez@gmail.com>
2022-03-14 19:59:42 -0400

    update for NWDC 2

--- R/11_NWDC.R
+++ R/11_NWDC.R
@@ -1,11 +1,16 @@
 rm(list = ls())
 library(googlesheets4)
 library(tidyverse)
-library()
+library(lubridate)
+# install using
+# remotes::install_github("uclalawcovid19behindbars/behindbarstools")
+library(behindbarstools)
 gs4_deauth()
 gs4_user()
+# url of the tacoma flight data
 flight_paths_url <- "1mmcB4pl8-RS0KczOSUz2HdpkqBo4AJQHjp-f4PgcvDk"
 
+# vera institute ice wide data
 covid_df <- "https://raw.githubusercontent.com/vera-institute/ice-detention" %>%
     str_c("-covid/master/data_daily/national_cases_daily.csv") %>%
     read_csv(col_types = cols()) %>%
@@ -14,11 +19,21 @@ covid_df <- "https://raw.githubusercontent.com/vera-institute/ice-detention" %>%
         Cases = cases_cumulative,
         Active = cases_current) %>%
     mutate(Date = as_date(Date)) %>%
+    # scale factor to match scale of arrivals
     mutate(Active = Active/ max(Active, na.rm = TRUE)*140) %>%
     mutate(Name = "Active\nCases")
 
-as_date(covid_df$Date)
+# ucla covid behind bars facility data
+hist_imm <- read_scrape_data(TRUE) %>%
+    filter(Jurisdiction == "immigration")
 
+# only look at tacoma nwdc data data
+tacoma_df <- hist_imm %>%
+    # this is the ID for NWDC
+    filter(Facility.ID == 1844) %>%
+    mutate(ColName="Active\nCases")
+
+# function for fixing strange dates in google sheet
 fix_dates <- function(D){
     unlist(lapply(D, function(x){
         if(is.null(x)){
@@ -31,19 +46,22 @@ fix_dates <- function(D){
         as_date()
 }
 
+# function for fixing weird number issues in google sheet
 fix_numeric <- function(X){
     unlist(lapply(X, function(x){
-        if(is.null(x)){
+        if(is.ntull(x)){
             return(NA_real_)
         }else{
             return(as.numeric(unlist(x)[1]))
         }}))
 }
 
+# read the flights data from the google sheet
 flights_df <- read_sheet(flight_paths_url, skip = 2) %>%
     mutate(Date = fix_dates(Date)) %>%
     filter(!is.na(Date))
 
+# plot flight data arrivals against scaled all ice active cases
 flights_df %>%
     mutate(`From Plane - Boarded Bus to NWDC...10` = fix_numeric(
         `From Plane - Boarded Bus to NWDC...10`
@@ -57,12 +75,14 @@ flights_df %>%
     theme_behindbars() +
     scale_color_bbdiscrete() +
     labs(color = "") +
+    xlim(ymd(c("2021-01-01", NA))) +
     ggtitle(
         "Active Cases & New Arrivals",
         str_c(
             "A Comparison of Outbreaks in ICE detention\ncenters ",
             "& new arrivals from Tacoma Airport"))
 
+# plot flight data arrivals against cases in specifically NWDC
 flights_df %>%
     mutate(`From Plane - Boarded Bus to NWDC...10` = fix_numeric(
         `From Plane - Boarded Bus to NWDC...10`
@@ -72,18 +92,16 @@ flights_df %>%
         `From Plane - Boarded Bus to NWDC...10`, na.rm = T)) %>%
     ggplot(aes(x=Date, y=Arrivals)) +
     geom_col() +
-    #geom_line(aes(x=Date, y = Active, color = Name), data = covid_df) +
+    geom_line(
+        aes(x=Date, y = Residents.Active, color = ColName), data = tacoma_df) +
     theme_behindbars() +
     scale_color_bbdiscrete() +
     labs(color = "") +
-    ggtitle("New Arrivals from Tacoma Airport")
-
-flights_df %>%
-    mutate(`From Plane - Boarded Bus to NWDC...10` = fix_numeric(
-        `From Plane - Boarded Bus to NWDC...10`
-    )) %>%
-    ggplot(aes(x=Date, y=`From Plane - Boarded Bus to NWDC...10`)) +
-    geom_col() +
-    facet_wrap(~`Previous Airport`)
+    xlim(ymd(c("2021-01-01", NA))) +
+    ggtitle(
+        "Active Cases & New Arrivals",
+        str_c(
+            "A Comparison of Outbreaks in NWDC\n ",
+            "& new arrivals from Tacoma Airport"))
 
 
